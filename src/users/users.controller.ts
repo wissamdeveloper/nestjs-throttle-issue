48f9c2b766ac761a46c3ef243996e8d8322631ae
Michael Hamill <michael.hamill3@gmail.com>
2024-03-02 09:16:26 -0500

    add skip throttle

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,7 +10,9 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { SkipThrottle } from '@nestjs/throttler';
 
+@SkipThrottle()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
